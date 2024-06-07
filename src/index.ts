a8833afe8d2c3732a03736bfc05b5b812033ff72
Alberto Pena <alberto.pena@tebesensor.com>
2024-06-07 19:11:51 -0300

    feat: initial commit
    Server running on port 5000
    
    get method on route /api/notes
    
    post method on route /api/notes
    
    put method on route /api/notes/:id

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import express from "express"
+import cors from "cors"
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/api/notes", async (req, res) => {
+    const notes = await prisma.note.findMany();
+    res.json(notes)
+})
+
+app.post("/api/notes", async (req, res) => {
+    const { title, content } = req.body;
+    if (!title || !content) {
+        return res.status(400).send("title and content fields required");
+    }
+    try {
+        const note = await prisma.note.create({
+            data: { title, content }
+        })
+        res.send(note)
+    }
+    catch (error) {
+        res.status(500).send("Error on server while creating note")
+    }
+})
+ 
+app.put("/api/notes/:id", async (req,res) => {
+    const { title, content } = req.body;
+    const id = parseInt(req.params.id);
+    if (!id || isNaN(id)) {
+        return res.status(400).send("ID must be a valid number");
+    } 
+    if (!title || !content) {
+        return res.status(400).send("title and content fields required");
+    }
+    try {
+        const note = await prisma.note.update({
+            data: { title, content },
+            where: {id: id}
+        })
+        res.status(201).send(note)
+    } catch (error) {
+        res.status(500).send("Error on server while updating note")
+    }
+})
+
+app.listen(5000, () => {
+    console.log("server running on localhost:5000")
+})
