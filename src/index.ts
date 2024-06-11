64021c1c2b99c3c75b148779f0320b88f76b0b0a
Alberto Pena <alberto.pena@tebesensor.com>
2024-06-10 22:01:46 -0300

    feat: add delete route for notes
    add delete route /api/notes/:id on index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,24 +29,39 @@ app.post("/api/notes", async (req, res) => {
         res.status(500).send("Error on server while creating note")
     }
 })
- 
-app.put("/api/notes/:id", async (req,res) => {
+
+app.put("/api/notes/:id", async (req, res) => {
     const { title, content } = req.body;
     const id = parseInt(req.params.id);
     if (!id || isNaN(id)) {
         return res.status(400).send("ID must be a valid number");
-    } 
+    }
     if (!title || !content) {
         return res.status(400).send("title and content fields required");
     }
     try {
         const note = await prisma.note.update({
             data: { title, content },
-            where: {id: id}
+            where: { id: id }
         })
         res.status(201).send(note)
     } catch (error) {
         res.status(500).send("Error on server while updating note")
+    }
+})
+
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = parseInt(req.params.id)
+    if (!id || isNaN(id)) {
+        return res.status(400).send("ID must be a valid number")
+    }
+    try {
+        await prisma.note.delete({
+            where: { id }
+        })
+        res.status(200).send("Note delete successfully")
+    } catch (error) {
+        res.status(500).send("Error on server while deleting note")
     }
 })
 
